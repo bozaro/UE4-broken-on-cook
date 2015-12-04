978e917efd86881603d9d644197e50314e263826
a.navrotskiy <navrotskiy@corp.mail.ru>
2015-12-04 13:37:02 +0300

    prepare for UDN

--- Game/Build.bat
+++ Game/Build.bat
@@ -1,52 +1,7 @@
 @echo off
-setlocal
-call %~dp0.variables.bat
+
+call "%UE4%\Engine\Build\BatchFiles\Build.bat" "GameEditor" Win64 Development "%~dp0Game.uproject"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-rem WO-4097
-call %~dp0.buildeditor.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-if "%BUILDCONFIG%" == "" (
-	set BUILDCONFIG=Development
-)
-
-if "%SERVER_BUILDCONFIG%" == "" (
-	set SERVER_BUILDCONFIG=Development
-)
-
-if "%TARGET_PLATFORMS%" == "" (
-	set TARGET_PLATFORMS=Win64
-)
-
-if "%SERVER_TARGET_PLATFORMS%" == "" (
-	set SERVER_TARGET_PLATFORMS=Win64
-)
-
-if "%NUM_COOKERS%" == "" (
-	set NUM_COOKERS=0
-)
-
-if "%MAPS%" == "" (
-	set MAPS=-allmaps
-) else (
-	set MAPS=-map=%MAPS%
-)
-
-if "%NOWINDOWSSERVER%" == "true" (
-	set SERVER_OPTIONS=
-) else (
-	set SERVER_OPTIONS=-server -serverconfig=%SERVER_BUILDCONFIG% -serverplatform=%SERVER_TARGET_PLATFORMS%
-)
-
-if "%NOCOOKALL%" == "true" (
-	set COOKALL=""
-) else (
-	set COOKALL="-CookAll"
-)
-set MAPS=-map=mini_arena
-set COOKALL=""
-set SERVER_OPTIONS=
-
-call "%UE4%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun "-project=%~dp0%PROJECT_NAME%.uproject" %UE4_ROCKET% -cook -NumCookersToSpawn=%NUM_COOKERS% %MAPS% -CrashReporter -stage -archive "-archivedirectory=%~dp0..\PackedGame" -package -clientconfig=%BUILDCONFIG% -targetplatform=%TARGET_PLATFORMS% -build -utf8output %SERVER_OPTIONS% -nobootstrapexe %COOKALL% %*
+call "%UE4%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=%~dp0Game.uproject -cook -stage -archive -archivedirectory=%~dp0..\PackedGame -package -clientconfig=Development -targetplatform=Win64 -build -utf8output
 if %errorlevel% neq 0 exit /b %errorlevel%
--- Game/GenerateProjectFiles.bat
+++ Game/GenerateProjectFiles.bat
@@ -1,10 +1,4 @@
 @echo off
-setlocal
-call %~dp0.variables.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
 
-cmd /v /c "%UE4%\Engine\Build\BatchFiles\CopyVisualizers.bat"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-"%UE4%\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -game %UE4_MODE% "%~dp0%PROJECT_NAME%.uproject" -2013
+%UE4%\Engine\Binaries\DotNET\UnrealBuildTool.exe -projectfiles -game %~dp0Game.uproject -engine
 if %errorlevel% neq 0 exit /b %errorlevel%
